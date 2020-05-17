c84376ac11e531de21b0cedb0c84cd8f0ac0f08c
trynetCo <49089039+trynetCo@users.noreply.github.com>
2020-05-17 12:26:01 +0300

    Kayıt ve Giriş

--- Örnekler/Örnek#2.py
+++ Örnekler/Örnek#2.py
@@ -5,7 +5,7 @@ b = input("Şifreni yaz: ")
 print("Başarıyla kayıt oldun \nŞimdi Giriş yap:") # "\n" bir alt satıra geçmek için kullanıyoruz
 c = input("Kullanıcı Adın: ") #Kullanıcıdan daha önceki bilgiyi istiyoruz
 d = input("Şifren: ")
-if (a != c or b != d): # "if" burda koşul anlamı ekliyor "!=" ise eşit değilse anlamına geliyor
+if (a != c or b != d): # "if" burda koşul anlamı ekliyor "!=" ise eşit değilse anlamına geliyor.
     #"or" ise bir bağlaç türkçedeki ve, veya bağlaçları ile aynı işlemde kullanılıyor ama ingilizcesi. Bilmiyorsanız araştıra bilirsiniz.
 
     print("Yanlış giriş yaplıdı lütfen tekrar deneyin")# Koşul doğru ise bu alt satıra geçiyor
