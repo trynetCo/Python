4e625fd0906d611020c0856ecb86c54d7dbfbc25
trynetCo <49089039+trynetCo@users.noreply.github.com>
2020-05-17 12:26:58 +0300

    Oyuncu bilgileri

--- Örnekler/Örnek#1.py
+++ Örnekler/Örnek#1.py
@@ -1,4 +1,4 @@
-ad = input("Oyuncu adı:") #Burda kullanıcıdan girdi(input) bekliyoruz bu girdi "ad"a ekleniyor
+ad = input("Oyuncu adı:") #Burda kullanıcıdan girdi(input) bekliyoruz bu girdi "ad"a ekleniyor.
 ozellik = input("Oyuncunun özelliği:")
 silahi = input("Oyuncunun silahı:")
 scl = input("Oyuncunun Puanı 1:")
@@ -6,8 +6,8 @@ scl2 = input("Oyuncunun Puanı 2:")
 scl3 = input("Oyuncunun Puanı 3:")
 
 print("         Oyuncu Bilgileri")
-print("Oyuncun adı:", ad) #Bu kısımda topladığımız verileri kullanarak bilgileri yazdırıyoruz
+print("Oyuncun adı:", ad) #Bu kısımda topladığımız verileri kullanarak bilgileri yazdırıyoruz.
 print("Oyuncun özelliği:",ozellik)
 print("Oyuncun silahı:", silahi)
 print("Oyuncun Toplam Puanı:", int(scl) + int(scl2) + int(scl3))
-#Yukardaki kısımda oyunucunun puanını toplamak istersek String yani yazı olarak toplayacağından değerimizi int ye dönüştürdük
+#Yukardaki kısımda oyunucunun puanını toplamak istersek String yani yazı olarak toplayacağından değerimizi int ye dönüştürdük.
